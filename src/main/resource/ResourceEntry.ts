485a2707b235944990279d5ac7cd06b946f263b4
jet2jet <34238471+jet2jet@users.noreply.github.com>
2020-01-19 00:28:05 +0900

    Fix some descriptions for `ResourceEntry`

--- src/main/resource/ResourceEntry.ts
+++ src/main/resource/ResourceEntry.ts
@@ -3,10 +3,25 @@ export interface ResourceEntryBaseType<
 	TID extends string | number,
 	TLang extends string | number
 > {
+	/**
+	 * The resource type name or numeric value.
+	 * For well-known type (such as `RT_ICON`), this value must be the predefined numeric value.
+	 */
 	type: TType;
+	/** The ID of resource data. */
 	id: TID;
+	/**
+	 * The language value of resource data.
+	 * According to specification, this value can be string, but
+	 * typically this value would be LANGID (numeric) value.
+	 */
 	lang: TLang;
+	/**
+	 * The code page value for strings within the resource data.
+	 * Typically this value would be the Unicode code page '1200'.
+	 */
 	codepage: number;
+	/** The actual resource data. */
 	bin: ArrayBuffer;
 	/** (used by output) */
 	offset?: number;
